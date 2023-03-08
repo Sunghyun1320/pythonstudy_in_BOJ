ce0ea955733bb9e7e7a0cb05b3c58d565a8e0e18
SungHyun1320 <7h1320@naver.com>
2023-03-08 21:16:16 +0900

    1162 solve

--- dijkstra/1162/1162.py
+++ dijkstra/1162/1162.py
@@ -10,32 +10,41 @@ city = [[] for _ in range(n+1)]
 
 for _ in range(m):
     a, b, cost = map(int, input().split())
+    if a == b:
+        continue
 
     city[a].append([b, cost])
     city[b].append([a, cost])
 
-distance = [INF for _ in range(n+1)]
-distance[1] = 0
+distance = [[INF for _ in range(k+1)] for _ in range(n+1)]
+distance[1] = [0 for _ in range(k+1)]
 
 visit = []
-heapq.heappush(visit, [0, 1, 0, INF])
+heapq.heappush(visit, [0, 1, 0])
 
 while visit:
-    new_cost, node, cover, cover_cost = heapq.heappop(visit)
+    new_cost, node, cover = heapq.heappop(visit)
+
+    # 이거 안해주면 시간초과..... 왜지?
+    # 시간을 줄일 수 있는건 맞는데 이걸 어떻게 떠올리냐....
+    if distance[node][cover] < new_cost:
+        continue
+
+    if cover + 1 <= k:
+        for next_node, cost in city[node]:
+            if distance[next_node][cover + 1] > new_cost:
+                distance[next_node][cover + 1] = new_cost
+                heapq.heappush(visit, [new_cost, next_node, cover + 1])
 
     for next_node, cost in city[node]:
-        if cover < k:
-            n_cost = new_cost
-            n_cover_cost = min(cover_cost, cost)
-
-        else:
-            n_cost = new_cost + min(cover_cost, cost)
-            n_cover_cost = max(cover_cost, cost)
-
-        if distance[next_node] <= n_cost:
+        if distance[next_node][cover] <= new_cost + cost:
             continue
 
-        distance[next_node] = n_cost
-        heapq.heappush(visit, [n_cost, next_node, cover + 1, n_cover_cost])
+        distance[next_node][cover] = new_cost + cost
+        heapq.heappush(visit, [new_cost + cost, next_node, cover])
 
-print(distance[n])
+ans = INF
+for i in range(k+1):
+    ans = min(ans, distance[n][i])
+
+print(ans)
