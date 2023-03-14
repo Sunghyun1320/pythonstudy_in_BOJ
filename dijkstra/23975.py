6f1e66d86e3b593b1f0f08ed704aa0aed9ba95eb
김성현 <7h1320@gmail.com>
2023-03-14 09:50:27 +0900

    save

--- dijkstra/23975.py
+++ dijkstra/23975.py
@@ -1,7 +1,7 @@
 import sys
 input = sys.stdin.readline
 import heapq
-INF = int(1e9)
+INF = int(1e10)
 #################################################
 n, m, k = map(int, input().split())
 
@@ -11,46 +11,56 @@ graph = [[] for _ in range(n+1)]
 
 for _ in range(m):
     a, b, c = map(int, input().split())
+    if a == b:
+        continue
 
     graph[a].append([b, c])
     graph[b].append([a, c])
 
 visit = []
-heapq.heappush(visit, [0, 1])
+heapq.heappush(visit, [0, 1, 0])
 
-distance = [[INF, 0] for _ in range(n+1)]
-visited = [False for _ in range(n+1)]
-visited[1] = True
+distance = [[INF, 0, 0] for _ in range(n+1)]
+# visited = [False for _ in range(n+1)]
+# visited[1] = True
 distance[1][0] = 0
 
 
 while visit:
-    cost, node = heapq.heappop(visit)
+    cost, node, root = heapq.heappop(visit)
+    if market[node] > 0:
+        root = node
 
     for next_node, dis in graph[node]:
         if dis + cost < distance[next_node][0]:
             distance[next_node][0] = dis + cost
             distance[next_node][1] = node
+            distance[next_node][2] = root
+            heapq.heappush(visit, [dis+cost, next_node, root])
 
         elif dis + cost == distance[next_node][0]:
             if distance[next_node][1] < node:
                 distance[next_node][1] = node
+                distance[next_node][2] = root
 
-        if visited[next_node]:
-            continue
-
-        visited[next_node] = True
-        heapq.heappush(visit, [dis+cost, next_node])
-
-
+        # if visited[next_node]:
+        #     continue
+        #
+        # visited[next_node] = True
+check = [True for _ in range(n+1)]
 for _ in range(k):
-    now_mart = int(input())
-    if distance[now_mart][0] == INF:
+    n = int(input())
+    now_mart = n
+    path = [now_mart]
+    if distance[now_mart][0] == INF or not check:
         print(-1)
         continue
 
     while True:
-        if now_mart == 0:
+        if now_mart == 0 or not check:
+            for node in path:
+                check[node] = False
+            print(-1)
             break
 
         if market[now_mart] > 0:
@@ -58,11 +68,6 @@ for _ in range(k):
             print(now_mart)
             break
 
-        now_mart = distance[now_mart][1]
-
-    if now_mart == 0:
-        print(-1)
-
-
-
-
+        now_mart = distance[now_mart][2]
+        distance[n][2] = now_mart
+        path.append(now_mart)
